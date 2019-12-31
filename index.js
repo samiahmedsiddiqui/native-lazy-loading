397b6e60dd85ca04cd87eb6cc3c7d2768835d4b2
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2019-12-31 18:43:10 +0500

    Compatibility with unsupported versions

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 const parser = require('posthtml-parser');
 const render = require('posthtml-render');
 
-function contentTree(treeObj, applyOpt) {
+function contentTree(treeObj, applyOpt, compatibility, library) {
+  const addClass = [];
+  const loadingClass = 'native-lazy-loading';
+
+  var checkCompClass = 0;
   var classes = [];
   var classesLength = 0;
   var className = '';
@@ -16,7 +20,7 @@ function contentTree(treeObj, applyOpt) {
     contentLength = treeObj.content.length;
     for (loopInit = 0; loopInit < contentLength; loopInit += 1) {
       if (treeObj.content[loopInit].tag) {
-        treeObj.content[loopInit] = contentTree(treeObj.content[loopInit], applyOpt);
+        treeObj.content[loopInit] = contentTree(treeObj.content[loopInit], applyOpt, compatibility, library);
       }
     }
   }
@@ -24,33 +28,62 @@ function contentTree(treeObj, applyOpt) {
   if (treeObj.tag) {
     tagName = treeObj.tag.toLowerCase();
     if (tagName === 'iframe' || tagName === 'img') {
-      if (!treeObj.attrs.loading) {
-        if (treeObj.attrs.class) {
-          classes = treeObj.attrs.class.split(' ');
-          classesLength = classes.length;
-          for (loopInit = 0; loopInit < classesLength; loopInit += 1) {
-            className = classes[loopInit];
-            if (className === '') {
-              continue;
-            }
+      if (treeObj.attrs.class) {
+        classes = treeObj.attrs.class.replace(/\s\s+/g, ' ').split(' ');
+        classesLength = classes.length;
+      }
 
-            if (applyOpt.auto && typeof applyOpt.auto === 'object' && applyOpt.auto[className]) {
-              loadingValue = 'auto';
-            } else if (applyOpt.lazy && typeof applyOpt.lazy === 'object' && applyOpt.lazy[className]) {
-              loadingValue = 'lazy';
-            } else if (applyOpt.eager && typeof applyOpt.eager === 'object' && applyOpt.eager[className]) {
-              loadingValue = 'eager';
-            }
+      if (!treeObj.attrs.loading) {
+        for (loopInit = 0; loopInit < classesLength; loopInit += 1) {
+          className = classes[loopInit];
+
+          if (applyOpt.auto && typeof applyOpt.auto === 'object' && applyOpt.auto[className]) {
+            loadingValue = 'auto';
+          } else if (applyOpt.lazy && typeof applyOpt.lazy === 'object' && applyOpt.lazy[className]) {
+            loadingValue = 'lazy';
+          } else if (applyOpt.eager && typeof applyOpt.eager === 'object' && applyOpt.eager[className]) {
+            loadingValue = 'eager';
           }
         }
 
         if (loadingValue !== '') {
           treeObj.attrs.loading = loadingValue;
-          if (treeObj.attrs.class) {
-            treeObj.attrs.class += ' native-lazy-loading';
-          } else {
-            treeObj.attrs.class = 'native-lazy-loading';
+          addClass.push(loadingClass);
+        }
+      }
+
+      if (compatibility && compatibility === true) {
+        if (library.notAllowed.length > 0) {
+          for (loopInit = 0; loopInit < classesLength; loopInit += 1) {
+            if (library.notAllowed[loopInit] === classes[loopInit]) {
+              checkCompClass = 1;
+              break;
+            }
           }
+        }
+
+        if (checkCompClass === 0) {
+          if (!treeObj.attrs['data-src'] && treeObj.attrs.src) {
+            treeObj.attrs['data-src'] = treeObj.attrs.src;
+
+            if (library.newSrc && library.newSrc !== '') {
+              treeObj.attrs.src = library.newSrc;
+            } else {
+              delete treeObj.attrs.src;
+            }
+          }
+
+          if (library.class && library.class !== '') {
+            addClass.push(library.class);
+          }
+        }
+      }
+
+      if (addClass.length > 0) {
+        if (treeObj.attrs.class) {
+          treeObj.attrs.class += ' ' + addClass.join(' ');
+        } else {
+          treeObj.attrs.class = addClass.join(' ');
         }
       }
     }
@@ -59,14 +92,19 @@ function contentTree(treeObj, applyOpt) {
   return treeObj;
 }
 
-function nativeLazyLoading(html, options,   ) {
+function nativeLazyLoading(html, options, compatibility, library) {
   if (typeof html !== 'undefined' && html !== '') {
     const htmlTree = parser(html);
     const treeLength = htmlTree.length;
 
-
+    var checkComp = false;
     var classLength = 0;
     var className = '';
+    var lib = {
+      class: 'lazyload',
+      newSrc: 'loading.gif',
+      notAllowed: []
+    };
     var loopInit = 0;
     var treeOptions = {
       defaultValue: 'lazy'
@@ -111,9 +149,27 @@ function nativeLazyLoading(html, options,   ) {
       }
     }
 
+    if (typeof compatibility === 'boolean') {
+      checkComp = compatibility;
+    }
+
+    if (library && typeof library === 'object') {
+      if (library.class) {
+        lib.class = library.class;
+      }
+
+      if (library.newSrc) {
+        lib.newSrc = library.newSrc;
+      }
+
+      if (library.notAllowed && library.notAllowed.length > 0) {
+        lib.notAllowed = library.notAllowed;
+      }
+    }
+
     for (loopInit = 0; loopInit < treeLength; loopInit += 1) {
       if (htmlTree[loopInit].tag) {
-        htmlTree[loopInit] = contentTree(htmlTree[loopInit], treeOptions);
+        htmlTree[loopInit] = contentTree(htmlTree[loopInit], treeOptions, checkComp, lib);
       }
     }
 
