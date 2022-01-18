b5bf56a2c8dc84bb389c9986198593bd0d0ba29b
Sami Ahmed Siddiqui <sami.siddiqui@yasglobal.com>
2022-01-18 16:42:21 +0500

    Add TS support

--- index.js
+++ index.js
@@ -1,189 +1,180 @@
-'use strict';
-
-const parser = require('posthtml-parser').default;
-const render = require('posthtml-render');
-
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const htmlParser = __importStar(require("posthtml-parser"));
+const posthtml_render_1 = require("posthtml-render");
 function contentTree(treeObj, applyOpt, compatibility, library) {
-  const addClass = [];
-  const defaultClass = 'native-lazy-loading';
-  const loadingClass = 'lazy-loading';
-
-  var checkCompClass = 0;
-  var classes = [];
-  var classesLength = 0;
-  var className = '';
-  var contentLength = 0;
-  var loopInit = 0;
-  var loopNotAllowed = 0;
-  var loadingValue = applyOpt.defaultValue;
-  var notAllowedLength = 0;
-  var tagName = '';
-
-  if (treeObj.content) {
-    contentLength = treeObj.content.length;
-    for (loopInit = 0; loopInit < contentLength; loopInit += 1) {
-      if (treeObj.content[loopInit].tag) {
-        treeObj.content[loopInit] = contentTree(treeObj.content[loopInit], applyOpt, compatibility, library);
-      }
-    }
-  }
-
-  if (treeObj.tag) {
-    tagName = treeObj.tag.toLowerCase();
-    if (tagName === 'iframe' || tagName === 'img') {
-      if (treeObj.attrs.class) {
-        classes = treeObj.attrs.class.replace(/\s\s+/g, ' ').split(' ');
-        classesLength = classes.length;
-      }
-
-      if (!treeObj.attrs.loading) {
-        for (loopInit = 0; loopInit < classesLength; loopInit += 1) {
-          className = classes[loopInit];
-
-          if (applyOpt.auto && typeof applyOpt.auto === 'object' && applyOpt.auto[className]) {
-            loadingValue = 'auto';
-          } else if (applyOpt.lazy && typeof applyOpt.lazy === 'object' && applyOpt.lazy[className]) {
-            loadingValue = 'lazy';
-          } else if (applyOpt.eager && typeof applyOpt.eager === 'object' && applyOpt.eager[className]) {
-            loadingValue = 'eager';
-          }
+    const addClass = [];
+    const defaultClass = 'native-lazy-loading';
+    const loadingClass = 'lazy-loading';
+    let checkCompClass = 0;
+    let classes = [];
+    let classesLength = 0;
+    if (treeObj.content) {
+        let contentLength = 0;
+        if (typeof treeObj.content === 'object') {
+            contentLength = treeObj.content.length;
         }
-
-        if (loadingValue !== '') {
-          treeObj.attrs.loading = loadingValue;
-          addClass.push(loadingClass);
-        }
-      }
-
-      if (compatibility && compatibility === true) {
-        if (library.notAllowed.length > 0) {
-          notAllowedLength = library.notAllowed.length;
-          for (loopInit = 0; loopInit < classesLength; loopInit += 1) {
-            for (loopNotAllowed = 0; loopNotAllowed < notAllowedLength; loopNotAllowed += 1) {
-              if (library.notAllowed[loopNotAllowed] === classes[loopInit]) {
-                checkCompClass = 1;
-                break;
-              }
+        for (let loopInit = 0; loopInit < contentLength; loopInit += 1) {
+            if (treeObj.content[loopInit] && treeObj.content[loopInit].tag) {
+                treeObj.content[loopInit] = contentTree(treeObj.content[loopInit], applyOpt, compatibility, library);
             }
-          }
         }
-
-        if (checkCompClass === 0) {
-          if (!treeObj.attrs['data-src'] && treeObj.attrs.src) {
-            treeObj.attrs['data-src'] = treeObj.attrs.src;
-
-            if (library.newSrc && library.newSrc !== '') {
-              treeObj.attrs.src = library.newSrc;
-            } else {
-              delete treeObj.attrs.src;
-            }
-          }
-
-          if (library.class && library.class !== '') {
-            addClass.push(library.class);
-          }
-        }
-      }
-
-      if (addClass.length > 0) {
-        addClass.unshift(defaultClass);
-        if (treeObj.attrs.class) {
-          treeObj.attrs.class += ' ' + addClass.join(' ');
-        } else {
-          treeObj.attrs.class = addClass.join(' ');
-        }
-      }
     }
-  }
-
-  return treeObj;
+    if (treeObj.tag) {
+        let tagName = treeObj.tag;
+        if (typeof tagName === 'string') {
+            tagName = tagName.toLowerCase();
+        }
+        if (!treeObj.attrs) {
+            treeObj.attrs = {};
+        }
+        if (tagName === 'iframe' || tagName === 'img') {
+            if (treeObj.attrs.class && typeof treeObj.attrs.class === 'string') {
+                classes = treeObj.attrs.class.replace(/\s\s+/g, ' ').split(' ');
+                classesLength = classes.length;
+            }
+            if (!treeObj.attrs.loading) {
+                let loadingValue = applyOpt.defaultValue;
+                for (let loopInit = 0; loopInit < classesLength; loopInit += 1) {
+                    const className = classes[loopInit];
+                    if (applyOpt.auto && applyOpt.auto.indexOf(className) !== -1) {
+                        loadingValue = 'auto';
+                    }
+                    else if (applyOpt.lazy && applyOpt.lazy.indexOf(className) !== -1) {
+                        loadingValue = 'lazy';
+                    }
+                    else if (applyOpt.eager && applyOpt.eager.indexOf(className) !== -1) {
+                        loadingValue = 'eager';
+                    }
+                }
+                if (loadingValue !== '') {
+                    treeObj.attrs.loading = loadingValue;
+                    addClass.push(loadingClass);
+                }
+            }
+            if (compatibility && compatibility === true) {
+                if (library.notAllowed && library.notAllowed.length > 0) {
+                    const notAllowedLength = library.notAllowed.length;
+                    for (let loopInit = 0; loopInit < classesLength; loopInit += 1) {
+                        for (let loopNotAllowed = 0; loopNotAllowed < notAllowedLength; loopNotAllowed += 1) {
+                            if (library.notAllowed[loopNotAllowed] === classes[loopInit]) {
+                                checkCompClass = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (checkCompClass === 0) {
+                    if (!treeObj.attrs['data-src'] && treeObj.attrs.src) {
+                        treeObj.attrs['data-src'] = treeObj.attrs.src;
+                        if (library.newSrc && library.newSrc !== '') {
+                            treeObj.attrs.src = library.newSrc;
+                        }
+                        else {
+                            delete treeObj.attrs.src;
+                        }
+                    }
+                    if (library.class && library.class !== '') {
+                        addClass.push(library.class);
+                    }
+                }
+            }
+            if (addClass.length > 0) {
+                addClass.unshift(defaultClass);
+                if (treeObj.attrs.class) {
+                    treeObj.attrs.class += ' ' + addClass.join(' ');
+                }
+                else {
+                    treeObj.attrs.class = addClass.join(' ');
+                }
+            }
+        }
+    }
+    return treeObj;
 }
-
 function nativeLazyLoading(html, options, compatibility, library) {
-  if (typeof html !== 'undefined' && html !== '') {
-    const htmlTree = parser(html);
-    const treeLength = htmlTree.length;
-
-    var checkComp = false;
-    var classLength = 0;
-    var className = '';
-    var lib = {
-      class: 'lazyload',
-      newSrc: '',
-      notAllowed: []
-    };
-    var loopInit = 0;
-    var treeOptions = {
-      defaultValue: 'lazy'
-    };
-
-    if (typeof options === 'object') {
-      if (typeof options.defaultValue === 'string') {
-        treeOptions.defaultValue = options.defaultValue;
-      }
-
-      if (options.auto && typeof options.auto === 'object' && options.auto[0]) {
-        classLength = options.auto.length;
-        if (classLength > 0) {
-          treeOptions.auto = [];
-          for (loopInit = 0; loopInit < classLength; loopInit += 1) {
-            className = options.auto[loopInit];
-            treeOptions.auto[className] = className;
-          }
+    if (typeof html !== 'undefined' && html !== '') {
+        const htmlTree = htmlParser.parser(html);
+        const lib = {
+            class: 'lazyload',
+            newSrc: '',
+            notAllowed: []
+        };
+        const treeOptions = {
+            defaultValue: 'lazy'
+        };
+        let checkComp = false;
+        let classLength = 0;
+        let loopInit = 0;
+        if (typeof options === 'object') {
+            if (typeof options.defaultValue === 'string') {
+                treeOptions.defaultValue = options.defaultValue;
+            }
+            if (options.auto && typeof options.auto === 'object' && options.auto.length > 0) {
+                classLength = options.auto.length;
+                treeOptions.auto = [];
+                for (loopInit = 0; loopInit < classLength; loopInit += 1) {
+                    treeOptions.auto.push(options.auto[loopInit]);
+                }
+            }
+            if (options.lazy && typeof options.lazy === 'object' && options.lazy.length > 0) {
+                classLength = options.lazy.length;
+                treeOptions.lazy = [];
+                for (loopInit = 0; loopInit < classLength; loopInit += 1) {
+                    treeOptions.lazy.push(options.lazy[loopInit]);
+                }
+            }
+            if (options.eager && typeof options.eager === 'object' && options.eager.length > 0) {
+                classLength = options.eager.length;
+                treeOptions.eager = [];
+                for (loopInit = 0; loopInit < classLength; loopInit += 1) {
+                    const className = options.eager[loopInit];
+                    treeOptions.eager.push(className);
+                }
+            }
         }
-      }
-
-      if (options.lazy && typeof options.lazy === 'object' && options.lazy[0]) {
-        classLength = options.lazy.length;
-        if (classLength > 0) {
-          treeOptions.lazy = [];
-          for (loopInit = 0; loopInit < classLength; loopInit += 1) {
-            className = options.lazy[loopInit];
-            treeOptions.lazy[className] = className;
-          }
+        if (typeof compatibility === 'boolean') {
+            checkComp = compatibility;
         }
-      }
-
-      if (options.eager && typeof options.eager === 'object' && options.eager[0]) {
-        classLength = options.eager.length;
-        if (classLength > 0) {
-          treeOptions.eager = [];
-          for (loopInit = 0; loopInit < classLength; loopInit += 1) {
-            className = options.eager[loopInit];
-            treeOptions.eager[className] = className;
-          }
+        if (library && typeof library === 'object') {
+            if (library.class) {
+                lib.class = library.class;
+            }
+            if (library.newSrc) {
+                lib.newSrc = library.newSrc;
+            }
+            if (library.notAllowed && library.notAllowed.length > 0) {
+                lib.notAllowed = library.notAllowed;
+            }
         }
-      }
+        const finalHTMLTree = htmlTree.map(function (treePart) {
+            let mappedPart = treePart;
+            if (typeof treePart === 'object') {
+                mappedPart = contentTree(treePart, treeOptions, checkComp, lib);
+            }
+            return mappedPart;
+        });
+        html = (0, posthtml_render_1.render)(finalHTMLTree);
     }
-
-    if (typeof compatibility === 'boolean') {
-      checkComp = compatibility;
-    }
-
-    if (library && typeof library === 'object') {
-      if (library.class) {
-        lib.class = library.class;
-      }
-
-      if (library.newSrc) {
-        lib.newSrc = library.newSrc;
-      }
-
-      if (library.notAllowed && library.notAllowed.length > 0) {
-        lib.notAllowed = library.notAllowed;
-      }
-    }
-
-    for (loopInit = 0; loopInit < treeLength; loopInit += 1) {
-      if (htmlTree[loopInit].tag) {
-        htmlTree[loopInit] = contentTree(htmlTree[loopInit], treeOptions, checkComp, lib);
-      }
-    }
-
-    html = render(htmlTree, options);
-  }
-
-  return html;
+    return html;
 }
-
 module.exports = nativeLazyLoading;
---
+++ test/lazy.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const nativeLazyLoading = require('../index');
+
+const testHTML = `<img src="https://www.yasglobal.com/wp-content/themes/yasglobal/images-cus/logo.svg" alt="YAS Global Logo" title="YAS Global Logo" class="no-lazy"/>
+<div class="white logos">
+  <div class="container">
+    <div class="testimonials">
+      <div class="item">
+        <img src="/wordpress-logo.svg" style="margin-top: -9px;" alt="WordPress Logo" title="WordPress Logo" class="testing-lazyload" />
+      </div>
+      <div class="item">
+        <img src="/drupal-logo.svg" style="margin-top: -9px;" alt="Drupal Logo" title="Drupal Logo" class="testing-lazyload" />
+      </div>
+      <div class="item">
+        <img src="/contentful-logo.svg" style="margin-top: -9px;" alt="Contentful Logo" title="Contentful Logo" class="testing-lazyload" />
+      </div>
+      <div class="item">
+        <img src="/contentstack-logo.svg" style="margin-top: -9px;" alt="ContentStack Logo" title="ContentStack Logo" />
+      </div>
+      <div class="item">
+        <img src="/netlify-logo.svg" style="margin-top: -9px;" alt="Netlify Logo" title="Netlify Logo" class="testing-lazyload" />
+      </div>
+      <div class="item">
+        <img src="/heroku-logo.svg" style="margin-top: -9px;" alt="Heroku Logo" title="Heroku Logo" class="testing-lazyload" />
+      </div>
+      <div class="item">
+        <img src="/pantheon-logo.svg" style="margin-top: -9px;" alt="Pantheon Logo" title="Pantheon Logo" />
+      </div>
+      <div class="item">
+        <img src="/wpengine-logo.svg" style="margin-top: -9px;" alt="WP Engine Logo" title="WP Engine Logo" class="testing-lazyload" />
+      </div>
+    </div>
+
+    <iframe src="https://www.w3schools.com" name="w3schools" class="testing-lazyload"></iframe>
+  </div>
+</div>`;
+
+describe('Test `loading="lazy"` attribute', () => {
+  it('Add loading="lazy" attribute on all images and iFrames', () => {
+    const expectedOutput = fs.readFileSync(
+      path.resolve(__dirname, 'expected-output/lazy-all.html'),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          console.error(err)
+          return;
+        }
+
+        return data;
+      }
+    );
+
+    assert.strictEqual(nativeLazyLoading(testHTML), expectedOutput);
+  });
+
+  it('Add loading="lazy" attribute on all images and iFrames except that contains no-lazy or header-images class', () => {
+    const expectedOutput = fs.readFileSync(
+      path.resolve(__dirname, 'expected-output/lazy-exception.html'),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          console.error(err)
+          return;
+        }
+
+        return data;
+      }
+    );
+
+    assert.strictEqual(nativeLazyLoading(testHTML, {
+      auto: ['no-lazy', 'header-images']
+    }), expectedOutput);
+  });
+
+  it('Add loading="lazy" attribute on only images and iFrames that contains testing-lazyload class', () => {
+    const expectedOutput = fs.readFileSync(
+      path.resolve(__dirname, 'expected-output/particular-lazy.html'),
+      'utf8',
+      (err, data) => {
+        if (err) {
+          console.error(err)
+          return;
+        }
+
+        return data;
+      }
+    );
+
+    assert.strictEqual(nativeLazyLoading(testHTML, {
+      lazy: ['testing-lazyload'],
+      defaultValue: ''
+    }), expectedOutput);
+  });
+});
